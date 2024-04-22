fd1d518868fdacbd626bbef0c34f6c77d0f7bb00
Dima <dima@current.ai>
2024-04-22 19:30:34 +0200

    Ensured that the `dlib`-s are unloaded during graceful shutdown.

--- src/dlib_ext.h
+++ src/dlib_ext.h
@@ -2,4 +2,7 @@
 
 #include <string>
 
+extern "C" void OnLoad();
+extern "C" void OnUnload();
+
 extern "C" std::string foo();
--- src/dlib_ext_boo.cc
+++ src/dlib_ext_boo.cc
@@ -1,1 +1,5 @@
-// Dummy, no `foo()` exposed.
+// No `foo()` exposed, but print loading / unloading msgs!
+#include <iostream>
+
+extern "C" void OnLoad() { std::cout << "dlib_ext_boo::OnLoad()" << std::endl; }
+extern "C" void OnUnload() { std::cout << "dlib_ext_boo::OnUnload()" << std::endl; }
