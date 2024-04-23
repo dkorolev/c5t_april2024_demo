0d0079061b6037e0a1d35b2136651900722a6fb0
Dima <dima@current.ai>
2024-04-23 11:17:18 +0200

    Trivial `wss`+`dlib` integration, works.

--- src/wss.cc
+++ src/wss.cc
@@ -5,7 +5,9 @@
 #include "bricks/strings/split.h"
 #include "bricks/sync/waitable_atomic.h"
 #include "lib_c5t_lifetime_manager.h"
+#include "lib_c5t_logger.h"
 #include "lib_c5t_popen2.h"  // IWYU pragma: keep
+#include "lib_c5t_dlib.h"
 #include "lib_build_info.h"
 
 inline std::string BasePathOf(std::string const& s) {
@@ -24,6 +26,17 @@ DEFINE_string(html_src, BasePathOf(__FILE__) + "/" + kHtmlSourceFile, "");
 
 int main(int argc, char** argv) {
   ParseDFlags(&argc, &argv);
+
+  std::string const bin_path = []() {
+    std::string const argv0 = current::Singleton<dflags::Argv0Container>().argv_0;
+    std::vector<std::string> argv0_path = current::strings::Split(argv0, current::FileSystem::GetPathSeparator());
+    argv0_path.pop_back();
+    std::string const res = current::strings::Join(argv0_path, current::FileSystem::GetPathSeparator());
+    return argv0[0] == current::FileSystem::GetPathSeparator() ? "/" + res : res;
+  }();
+
+  C5T_DLIB_SET_BASE_DIR(bin_path);
+  C5T_LOGGER_SET_LOGS_DIR(bin_path);
 
   try {
     if (current::FileSystem::GetFileSize(FLAGS_python_src) == 0u) {
@@ -81,6 +94,35 @@ int main(int argc, char** argv) {
       current::net::http::Headers({{"Location", "/up?from=stop"}, {"Cache-Control", "no-store, must-revalidate"}}),
       current::net::constants::kDefaultHTMLContentType);
     time_to_stop_http_server_and_die.SetValue(true);
+  });
+
+  routes += http.Register("/dlib", [](Request r) {
+    std::ostringstream oss;
+    int n = 0u;
+    C5T_DLIB_LIST([&oss, &n](std::string const& s) {
+      oss << ',' << s;
+      ++n;
+    });
+    if (!n) {
+      r("no dlibs loaded\n");
+    } else {
+      r(oss.str().substr(1));
+    }
+  });
+
+  routes += http.Register("/dlib", URLPathArgs::CountMask::One, [](Request r) {
+    std::string const name = r.url_path_args[0];
+    C5T_DLIB_USE(
+        name,
+        [&r](C5T_DLib& dlib) {
+          auto const s = dlib.Call<std::string()>("foo");
+          if (Exists(s)) {
+            r("has foo(): " + Value(s) + '\n');
+          } else {
+            r("no foo()\n");
+          }
+        },
+        [&r]() { r("no such dlib\n"); });
   });
 
   C5T_LIFETIME_MANAGER_TRACKED_THREAD(
