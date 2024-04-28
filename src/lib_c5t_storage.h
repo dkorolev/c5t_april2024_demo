d2da32c5ba33a2a19abe860fe0356d699eeefeb1
Dima <dima@current.ai>
2024-04-28 13:04:30 +0200

    Incremental improvements for Storage to make it more `pimpl` and less header-only.

--- src/lib_c5t_storage.h
+++ src/lib_c5t_storage.h
@@ -18,9 +18,21 @@ struct C5T_STORAGE_Layer {
 };
 
 // TODO: unsure of this is needed at all
-struct C5T_STORAGE_FIELD_Interface {
+class C5T_STORAGE_FIELD_Interface {
  protected:
   C5T_STORAGE_FIELD_Interface() = default;
+
+ public:
+  ~C5T_STORAGE_FIELD_Interface() = default;
+
+  virtual std::string const& Name() = 0;
+
+  // `void*` is `T*`.
+  virtual std::string DoSerializeImpl(void const*) const = 0;
+  virtual bool DoDeserializeImpl(std::string const&, void*) const = 0;
+
+  // TODO: move this into the storage layer?
+  virtual void* GetMapAsVoidPtr(C5T_STORAGE_FIELD_Interface const&) = 0;
 };
 
 // TODO: syntax
@@ -31,6 +43,8 @@ class C5T_STORAGE_Interface {
  public:
   virtual ~C5T_STORAGE_Interface() = default;
   virtual C5T_STORAGE_Layer& StorageLayerForField(C5T_STORAGE_FIELD_Interface const&) = 0;
+
+  // Stateless, effectively static, inner methods.
   virtual void DoSave(std::string const& field, std::string const& key, std::string const& value) = 0;
   virtual Optional<std::string> DoLoad(std::string const& field, std::string const& key) = 0;
   virtual void DoDelete(std::string const& field, std::string const& key) = 0;
@@ -42,131 +56,154 @@ std::unique_ptr<C5T_STORAGE_Interface> C5T_STORAGE_CREATE_UNIQUE_INSANCE(std::st
 // Throws `StorageNotInitializedException` if neither `CREATE`-d nor `INJECT`-ed.
 C5T_STORAGE_Interface& C5T_STORAGE_INSTANCE();
 
+template <class T>
+struct C5T_STORAGE_FIELD_TYPES {
+  using rhs_t = std::pair<bool, std::unique_ptr<T>>;  // { loaded, value }
+  using map_t = std::unordered_map<std::string, rhs_t>;
+};
+
 // TODO: maybe make the template type inner, so that `C5T_STORAGE_FIELD` can be passed around?
+template <class T>
+class C5T_STORAGE_FIELD_ACCESSOR final {
+ private:
+  C5T_STORAGE_FIELD_Interface& self;
+  typename C5T_STORAGE_FIELD_TYPES<T>::map_t& contents;
+  C5T_STORAGE_Interface& impl;
+
+  std::unique_ptr<T>& InnerGet(std::string key) const {
+    auto& p = contents[key];
+    if (!p.first) {
+      p.first = true;
+      Optional<std::string> s = impl.DoLoad(self.Name(), std::move(key));
+      if (Exists(s)) {
+        try {
+          // TODO: evolve
+          if (p.second) {
+            if (!self.DoDeserializeImpl(Value(s), p.second.get())) {
+              p.second = nullptr;
+            }
+          } else {
+            T instance;
+            if (self.DoDeserializeImpl(Value(s), &instance)) {
+              p.second = std::make_unique<T>(std::move(instance));
+            }
+          }
+        } catch (current::Exception const&) {
+          // TODO: log the error, test it
+        }
+      } else {
+        p.second = nullptr;
+      }
+    }
+    return p.second;
+  }
+
+  void InnerSet(std::string key, T const& value) const {
+    impl.DoSave(self.Name(), key, self.DoSerializeImpl(&value));
+    typename C5T_STORAGE_FIELD_TYPES<T>::rhs_t& p = contents[key];
+    p.first = true;
+    if (p.second) {
+      *p.second = value;
+    } else {
+      p.second = std::make_unique<T>(value);
+    }
+  }
+
+  void InnerDel(std::string const& key) const {
+    if (InnerGet(key)) {
+      contents[key].second = nullptr;
+      impl.DoDelete(self.Name(), key);
+    }
+  }
+
+ public:
+  C5T_STORAGE_FIELD_ACCESSOR(C5T_STORAGE_FIELD_Interface& self, C5T_STORAGE_Interface& impl)
+      : self(self),
+        contents(*reinterpret_cast<typename C5T_STORAGE_FIELD_TYPES<T>::map_t*>(self.GetMapAsVoidPtr(self))),
+        impl(impl) {
+    C5T_STORAGE_Layer& p = impl.StorageLayerForField(self);
+    if (!p.initialized) {
+      contents.clear();
+      p.initialized = true;
+    }
+  }
+
+  bool Has(std::string key) const { return InnerGet(std::move(key)) != nullptr; }
+
+  template <class E = StorageKeyNotFoundException, typename... ARGS>
+  T const& GetOrThrow(std::string key, ARGS&&... args) const {
+    auto const& p = InnerGet(std::move(key));
+    if (p != nullptr) {
+      return *p;
+    } else {
+      throw E(std::forward<ARGS>(args)...);
+    }
+  }
+
+  Optional<T> Get(std::string key) const {
+    auto const& p = InnerGet(std::move(key));
+    if (p != nullptr) {
+      return *p;
+    } else {
+      return nullptr;
+    }
+  }
+
+  template <class TT = T>
+  void Set(std::string key, TT&& value) {
+    InnerSet(std::move(key), std::forward<TT>(value));
+  }
+
+  void Del(std::string const& key) { InnerDel(key); }
+};
+
 template <typename T>
 class C5T_STORAGE_FIELD : public C5T_STORAGE_FIELD_Interface {
  private:
   C5T_STORAGE_FIELD() = delete;
-  using rhs_t = std::pair<bool, std::unique_ptr<T>>;  // { loaded, value }
   std::string name_;
-  mutable std::unordered_map<std::string, rhs_t> contents_;
+  mutable typename C5T_STORAGE_FIELD_TYPES<T>::map_t contents_;
 
  protected:
   C5T_STORAGE_FIELD(char const* name) : name_(name) { _C5T_STORAGE_DeclareField(this, name_); }
-  virtual std::string DoSerialize(T const&) const = 0;
-  virtual void DoDeserialize(std::string const&, std::unique_ptr<T>&) const = 0;
+
+  std::string const& Name() override { return name_; }
+  void* GetMapAsVoidPtr(C5T_STORAGE_FIELD_Interface const&) override { return &contents_; }
 
  public:
-  // TODO: move from the header file what can be moved to the source file
-  class Wrapper final {
-   private:
-    C5T_STORAGE_FIELD& self;
-    C5T_STORAGE_Interface& impl;
-
-    friend class C5T_STORAGE_FIELD;
-    Wrapper(C5T_STORAGE_FIELD& self, C5T_STORAGE_Interface& impl) : self(self), impl(impl) {
-      C5T_STORAGE_Layer& p = impl.StorageLayerForField(self);
-      if (!p.initialized) {
-        self.contents_.clear();
-        p.initialized = true;
-      }
-    }
-
-    std::unique_ptr<T>& InnerGet(std::string key) const {
-      auto& p = self.contents_[key];
-      if (!p.first) {
-        p.first = true;
-        Optional<std::string> s = impl.DoLoad(self.name_, std::move(key));
-        if (Exists(s)) {
-          try {
-            // TODO: evolve
-            self.DoDeserialize(Value(s), p.second);
-          } catch (current::Exception const&) {
-            // TODO: log the error, test it
-          }
-        } else {
-          p.second = nullptr;
-        }
-      }
-      return p.second;
-    }
-
-    void InnerSet(std::string key, T const& value) const {
-      impl.DoSave(self.name_, key, self.DoSerialize(value));
-      rhs_t& p = self.contents_[key];
-      p.first = true;
-      if (p.second) {
-        *p.second = value;
-      } else {
-        p.second = std::make_unique<T>(value);
-      }
-    }
-
-    void InnerDel(std::string const& key) const {
-      if (InnerGet(key)) {
-        self.contents_[key].second = nullptr;
-        impl.DoDelete(self.name_, key);
-      }
-    }
-
-   public:
-    bool Has(std::string key) const { return InnerGet(std::move(key)) != nullptr; }
-
-    template <class E = StorageKeyNotFoundException, typename... ARGS>
-    T const& GetOrThrow(std::string key, ARGS&&... args) const {
-      auto const& p = InnerGet(std::move(key));
-      if (p != nullptr) {
-        return *p;
-      } else {
-        throw E(std::forward<ARGS>(args)...);
-      }
-    }
-
-    Optional<T> Get(std::string key) const {
-      auto const& p = InnerGet(std::move(key));
-      if (p != nullptr) {
-        return *p;
-      } else {
-        return nullptr;
-      }
-    }
-
-    template <class TT = T>
-    void Set(std::string key, TT&& value) {
-      InnerSet(std::move(key), std::forward<TT>(value));
-    }
-
-    void Del(std::string const& key) { InnerDel(key); }
-  };
-
-  Wrapper operator()() { return Wrapper(*this, C5T_STORAGE_INSTANCE()); }
   // TODO: test `const`-ness.
-  Wrapper operator()() const { return Wrapper(*this, C5T_STORAGE_INSTANCE()); }
+  C5T_STORAGE_FIELD_ACCESSOR<T> operator()() { return C5T_STORAGE_FIELD_ACCESSOR<T>(*this, C5T_STORAGE_INSTANCE()); }
+  C5T_STORAGE_FIELD_ACCESSOR<T> operator()() const {
+    return C5T_STORAGE_FIELD_ACCESSOR<T>(*this, C5T_STORAGE_INSTANCE());
+  }
 };
 
 // TODO: evolve
 
-#define C5T_STORAGE_DECLARE_FIELD(name, type)                                      \
-  class C5T_STORAGE_FIELD_##name final : public C5T_STORAGE_FIELD<type> {          \
-   protected:                                                                      \
-    std::string DoSerialize(type const&) const override;                           \
-    void DoDeserialize(std::string const&, std::unique_ptr<type>&) const override; \
-                                                                                   \
-   public:                                                                         \
-    C5T_STORAGE_FIELD_##name() : C5T_STORAGE_FIELD(#name) {}                       \
-  };                                                                               \
+#define C5T_STORAGE_DECLARE_FIELD(name, type)                             \
+  class C5T_STORAGE_FIELD_##name final : public C5T_STORAGE_FIELD<type> { \
+   protected:                                                             \
+    std::string DoSerializeImpl(void const*) const override;              \
+    bool DoDeserializeImpl(std::string const&, void*) const override;     \
+                                                                          \
+   public:                                                                \
+    C5T_STORAGE_FIELD_##name() : C5T_STORAGE_FIELD(#name) {}              \
+  };                                                                      \
   extern C5T_STORAGE_FIELD_##name C5T_STORAGE_FIELD_INSTANCE_##name
 
 // NOTE: For `C5T_STORAGE_DEFINE_FIELD` and for `C5T_STORAGE_FIELD`, JSON & serialization need to be `#include`-d.
-#define C5T_STORAGE_DEFINE_FIELD(name, type, meta)                                                                     \
-  std::string C5T_STORAGE_FIELD_##name::DoSerialize(type const& x) const { return JSON<JSONFormat::Minimalistic>(x); } \
-  void C5T_STORAGE_FIELD_##name::DoDeserialize(std::string const& s, std::unique_ptr<type>& p) const {                 \
-    if (p) {                                                                                                           \
-      ParseJSON<type, JSONFormat::Minimalistic>(s, *p);                                                                \
-    } else {                                                                                                           \
-      p = std::make_unique<type>(ParseJSON<type, JSONFormat::Minimalistic>(s));                                        \
-    }                                                                                                                  \
-  }                                                                                                                    \
+#define C5T_STORAGE_DEFINE_FIELD(name, type, meta)                                        \
+  std::string C5T_STORAGE_FIELD_##name::DoSerializeImpl(void const* p) const {            \
+    return JSON<JSONFormat::Minimalistic>(*reinterpret_cast<type const*>(p));             \
+  }                                                                                       \
+  bool C5T_STORAGE_FIELD_##name::DoDeserializeImpl(std::string const& s, void* p) const { \
+    try {                                                                                 \
+      ParseJSON<type, JSONFormat::Minimalistic>(s, *reinterpret_cast<type*>(p));          \
+      return true;                                                                        \
+    } catch (current::Exception const&) {                                                 \
+      return false;                                                                       \
+    }                                                                                     \
+  }                                                                                       \
   C5T_STORAGE_FIELD_##name C5T_STORAGE_FIELD_INSTANCE_##name
 
 // To access storage fields.
